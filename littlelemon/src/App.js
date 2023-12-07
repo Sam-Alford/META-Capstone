f3dd0b59387173f598f3b1449ee2ad45355c60ca
Sam-Alford <salford21@gmail.com>
2023-12-06 19:28:48 -0600

    organization
    organization

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,34 +1,10 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
-import Homepage from './Homepage';
-import AboutMe from './AboutMe';
-import Contact from './Contact';
-import Menu from './Menu';
-import Reservations from './Reservations';
-import OrderOnline from './OrderOnline';
-import Login from './Login';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <nav className="nav">
-        <Link to="/" className="nav-item">Home</Link>
-        <Link to="/about-us" className="nav-item">About</Link>
-        <Link to="/contact" className="nav-item">Contact</Link>
-        <Link to="/menu" className="nav-item">Menu</Link>
-        <Link to="/reservations" className="nav-item">Reservations</Link>
-        <Link to="/order" className="nav-item">Order Online</Link>
-        <Link to="/login" className="nav-item">Login</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about-us" element={<AboutMe />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/reservations" element={<Reservations />} />
-        <Route path="/order" element={<OrderOnline />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Nav />
     </div>
   );
 }
---
+++ littlelemon/src/components/Nav.js
@@ -0,0 +1,36 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Homepage from '../pages/Homepage';
+import AboutMe from '../pages/AboutMe';
+import Contact from '../pages/Contact';
+import Menu from '../pages/Menu';
+import Reservations from '../pages/Reservations';
+import OrderOnline from '../pages/OrderOnline';
+import Login from '../pages/Login';
+
+function Nav() {
+    return (
+        <div>
+            <nav className="nav">
+                <image>logo</image>
+                <Link to="/" className="nav-item">Home</Link>
+                <Link to="/about-us" className="nav-item">About</Link>
+                <Link to="/contact" className="nav-item">Contact</Link>
+                <Link to="/menu" className="nav-item">Menu</Link>
+                <Link to="/reservations" className="nav-item">Reservations</Link>
+                <Link to="/order" className="nav-item">Order Online</Link>
+                <Link to="/login" className="nav-item">Login</Link>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/about-us" element={<AboutMe />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/reservations" element={<Reservations />} />
+                <Route path="/order" element={<OrderOnline />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Nav
