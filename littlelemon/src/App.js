3718ab0d216154af56f7be7c9a0fdca85e2b9f7b
Sam-Alford <salford21@gmail.com>
2023-12-06 18:49:17 -0600

    nav bar
    updated nav bar with couple of pages.

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Homepage from './Homepage';
+import AboutMe from './AboutMe';
+import Contact from './Contact';
 
 function App() {
   return (
     <div className="App">
-     Howdy
+      <nav className="nav">
+        <Link to="/" className="nav-item">Homepage</Link>
+        <Link to="/about-us" className="nav-item">About Little Lemon</Link>
+        <Link to="/contact" className="nav-item">Contact</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/about-us" element={<AboutMe />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
