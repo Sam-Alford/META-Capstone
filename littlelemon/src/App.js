f001e00f5e661d0c347555de1da3162070dc4ce8
Sam-Alford <salford21@gmail.com>
2023-12-09 20:11:41 -0600

    Update App.js
    Update

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Wrapper from './components/Wrapper'
 
 
 function App() {
   return (
-    <div>  
+    <Wrapper>  
         <Nav />
         <Footer />
-    </div>
+    </Wrapper>
  
   );
 }
